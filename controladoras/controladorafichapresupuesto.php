bc0c2726b2113a423b95112f8a089504e29bdbea
Jaime Acevedo <jaim.acevedoc@gmail.com>
2013-08-29 13:44:07 -0400

    Controladora ficha Presupuesto

--- controladoras/controladorafichapresupuesto.php
+++ controladoras/controladorafichapresupuesto.php
@@ -1,4 +1,9 @@
 <?php 
+require_once '../bdmysql/MySqlCon.php'; 
+require_once '../pojos/fichadental.php';
+require_once '../pojos/presupuesto.php';
+
+
 class ContrladoraFichaPresupuesto
 {
 	function crearFicha($idFicha, $fechaCreacion, $idPaciente, $Anamnesis)
@@ -9,29 +14,319 @@ class ContrladoraFichaPresupuesto
 	{
 
 	}
-	function modificarPresupuesto($presupuesto)
+	function insertarPresupuesto(Presupuesto $presupuesto)
+	{
+		$conexion = new MySqlCon();
+		$idFicha = $presupuesto->idFicha;
+		$ValorTotal = $presupuesto->ValorTotal;
+		try 
+	   	{ 	 
+	        $this->SqlQuery='';
+	        $this->SqlQuery='INSERT INTO `presupuesto` (`ID_PRESUPUESTO` ,`ID_FICHA` ,`VALORTOTAL`) VALUES (NULL ,  ?, ?);';
+	        $sentencia=$conexion->prepare($this->SqlQuery);
+	        $sentencia->bind_param('ii',$idFicha,$ValorTotal);
+	      	if($sentencia->execute())
+	      	{
+	        	$conexion->close();
+				return true;
+			}
+			else
+			{
+				$conexion->close();
+	        	return false;
+	        }
+        }
+    	catch(Exception $e)
+    	{
+         return false;
+         throw new $e("Error al Registrar Odontologo");
+        }
+	}
+	function insertarFicha(Ficha $ficha)
+	{
+		$conexion = new MySqlCon();
+		$idPaciente = $ficha->idPaciente;
+		$idOdontologo = $ficha->idOdontologo;
+		$fechaIngreso = $ficha->fechaIngreso;
+		$anamnesis = $ficha->anamnesis;
+		try 
+	   	{ 	 
+	        $this->SqlQuery='';
+	        $this->SqlQuery='INSERT INTO `fichadental` (`ID_FICHA`,`ID_PACIENTE`,`ID_ODONTOLOGO`,`FECH_INGRESO`,`ANAMNESIS`)
+	         VALUES (NULL , ?,  ?, ?,  ?);';
+	        $sentencia=$conexion->prepare($this->SqlQuery);
+	        $sentencia->bind_param('iiss',$idPaciente,$idOdontologo,$fechaIngreso,$anamnesis);
+	      	if($sentencia->execute())
+	      	{
+	        	$conexion->close();
+				return true;
+			}
+			else
+			{
+				$conexion->close();
+	        	return false;
+	        }
+        }
+    	catch(Exception $e)
+    	{
+         return false;
+         throw new $e("Error al Registrar Odontologo");
+        }
+	}
+	function modificarPresupuesto(Presupuesto $presupuesto)
+	{
+		$conexion = new MySqlCon();
+		$idPresupuesto = $presupuesto->idPresupuesto;
+		$idFicha = $presupuesto->idFicha;
+		$valorTotal = $presupuesto->valorTotal;
+		try 
+	   	{ 	 
+	        $this->SqlQuery='';
+	        $this->SqlQuery='UPDATE `presupuesto` SET `ID_FICHA` = ?, `VALORTOTAL` = ? WHERE `ID_PRESUPUESTO` = ?';
+	        $sentencia=$conexion->prepare($this->SqlQuery);
+	        $sentencia->bind_param('iii',$idFicha,$valorTotal,$idPresupuesto);
+	      	if($sentencia->execute())
+	      	{
+	        	$conexion->close();
+				return true;
+			}
+			else
+			{
+				$conexion->close();
+	        	return false;
+	        }
+        }
+    	catch(Exception $e)
+    	{
+         return false;
+         throw new $e("Error al Actualizar Usuarios");
+        }
+	}
+	function modificarFicha(Ficha $ficha)
 	{
 
+		$conexion = new MySqlCon();
+		$idFicha = $ficha->idFicha;
+		$idPaciente = $ficha->idPaciente;
+		$idOdontologo = $ficha->idOdontologo;
+		$fechaIngreso = $ficha->fechaIngreso;
+		$anamnesis = $ficha->anamnesis;
+		try 
+	   	{ 	 
+	        $this->SqlQuery='';
+	        $this->SqlQuery='UPDATE `fichadental` SET `ID_PACIENTE` = ?, `ID_ODONTOLOGO` = ?, FECH_INGRESO=?, ANAMNESIS=? WHERE `ID_FICHA` = ?';
+	        $sentencia=$conexion->prepare($this->SqlQuery);
+	        $sentencia->bind_param('iissi',$idPaciente,$idOdontologo,$fechaIngreso,$anamnesis,$idFicha);
+	      	if($sentencia->execute())
+	      	{
+	        	$conexion->close();
+				return true;
+			}
+			else
+			{
+				$conexion->close();
+	        	return false;
+	        }
+        }
+    	catch(Exception $e)
+    	{
+         return false;
+         throw new $e("Error al Actualizar Usuarios");
+        }
 	}
 	function desabilitarPresupuesto($presupuesto)
 	{
 
 	}
-	function listarPresupuesto($presupuesto)
+	function buscarPresupuestoPorId($idPresupuesto)
 	{
-
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT * FROM presupuesto WHERE ID_PRESUPUESTO=? ";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+		   	$sentencia->bind_param('i',$idPresupuesto);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idPresupuesto, $idFicha, $valorTotal);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$presupuesto = new Presupuesto();
+					$presupuesto->initClass($idPresupuesto, $idFicha, $valorTotal);
+        			$this->datos[$indice] = $presupuesto;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;	
 	}
-	function modificarFicha($ficha)
+	function buscarFichaPorId($idFicha)
 	{
-
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT * FROM fichadental WHERE ID_FICHA=?";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+		   	$sentencia->bind_param('i',$idFicha);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$ficha = new Ficha();
+					$ficha->initClass($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);
+        			$this->datos[$indice] = $presupuesto;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;
 	}
+	function buscarFichaPorIdPersona($idPersona)
+	{
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT * FROM fichadental WHERE ID_PACIENTE=?";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+		   	$sentencia->bind_param('i',$idPersona);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$ficha = new Ficha();
+					$ficha->initClass($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);
+        			$this->datos[$indice] = $presupuesto;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;
+	}
+	function buscarPresupuestoPorIdPersona($idPersona)
+	{
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT pe.* FROM presupuesto pe, fichadental fi WHERE fi.ID_PACIENTE=? AND fi.ID_FICHA = pe.ID_FICHA ";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+		   	$sentencia->bind_param('i',$idPersona);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idPresupuesto, $idFicha, $valorTotal);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$presupuesto = new Presupuesto();
+					$presupuesto->initClass($idPresupuesto, $idFicha, $valorTotal);
+        			$this->datos[$indice] = $presupuesto;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;
+	}
+	function listarPresupuesto()
+	{
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT * FROM `presupuesto`";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idPresupuesto, $idFicha, $valorTotal);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$presupuesto = new Presupuesto();
+					$presupuesto->initClass($idPresupuesto, $idFicha, $valorTotal);
+        			$this->datos[$indice] = $presupuesto;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;
+	}
+	function listarFicha()
+	{
+		$conexion = new MySqlCon();
+		$this->datos ='';
+		try
+		{
+			$this->SqlQuery = '';
+			$this->SqlQuery = "SELECT * FROM `fichadental`";
+		   	$sentencia=$conexion->prepare($this->SqlQuery);
+        	if($sentencia->execute())
+        	{
+        		$sentencia->bind_result($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);					
+				$indice=0;     
+				while($sentencia->fetch())
+				{
+					$ficha = new Ficha();
+					$ficha->initClass($idFicha, $idPaciente, $idPresupuesto, $idOdontologo, $fechaIngreso, $anamnesis);
+        			$this->datos[$indice] = $ficha;
+        			
+        			$indice++;
+				}
+      		}
+       		$conexion->close();
+    	}
+    	catch(Exception $e)
+    	{
+        	throw new $e("Error al listar pacientes");
+        }
+        return $this->datos;
+	}
+
 	function desabilitarFicha($ficha)
 	{
 
 	}
-	function listarFicha($ficha)
-	{
-
-	}
+	
 }
 ?>
