04f1ff101af42f93bc299c350b72c60c826cc1da
Jaime Acevedo <jaim.acevedoc@gmail.com>
2013-09-28 19:01:46 -0300

    Buscar
    Se integro un Buscar por Nombre, ademas se agrego Progress dialog por
    cada busqueda

--- Movil/SFH-Paciente/src/cl/sfh/controlaodas/ControladoraListaPrecios.java
+++ Movil/SFH-Paciente/src/cl/sfh/controlaodas/ControladoraListaPrecios.java
@@ -9,7 +9,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
 import cl.sfh.entidades.ListaPreciosEntidad;
 import cl.sfh.libreria.JSONParser;
 
@@ -39,7 +38,6 @@ public class ControladoraListaPrecios
 				listaPrecios.add(entidad);
 				
 			}
-			Log.e("arreglos", arregloJson.get(0)+"");
 		} 
 		catch (JSONException e)
 		{
@@ -49,18 +47,37 @@ public class ControladoraListaPrecios
 		
 		return listaPrecios;
 	}
-	public ArrayList<String> listarPrecios(int i)
+	public ArrayList<ListaPreciosEntidad> listarPreciosNombres(String nombre)
 	{
-		Log.e("Controladora", "Entre");
-		ArrayList<String> lista  = new ArrayList<String>();
-		JSONObject jsonEnviar = new JSONObject();
+		ArrayList<ListaPreciosEntidad> listaPrecios = new ArrayList<ListaPreciosEntidad>();
+		mensajeEnviar ="{\"indice\":4,\"nombre\":\""+nombre+"\"}";
 		JSONParser parser = new JSONParser();
 		List<NameValuePair> parametros = new ArrayList<NameValuePair>();
-		parametros.add(new BasicNameValuePair("send", "{\"indice\":3}"));
+		parametros.add(new BasicNameValuePair("send", mensajeEnviar));
 		JSONObject objetoJson = parser.makeHttpRequest("ws-precios-insumos.php",
 														"POST", parametros);
-		Log.e("Controladora", "Volvi");
-		lista.add(objetoJson.toString());
-		return lista;
+		try
+		{
+			JSONArray arregloJson = objetoJson.getJSONArray("listaPrecios");
+			for(int i=0;i<arregloJson.length();i++)
+			{
+				//{"idPrecios":4,"comentario":"Urgencia. Tratamiento Inicial 1 Sesion","valorNeto":14500,"valorIva":2755,"valorTotal":14500}
+				JSONObject objetoJsonFor = (JSONObject) arregloJson.get(i);
+				ListaPreciosEntidad entidad = new ListaPreciosEntidad(
+						objetoJsonFor.getInt("idPrecios"), objetoJsonFor.getString("comentario"),
+						objetoJsonFor.getInt("valorNeto"), objetoJsonFor.getInt("valorIva"),
+						objetoJsonFor.getInt("valorTotal"));
+				listaPrecios.add(entidad);
+				
+			}
+		} 
+		catch (JSONException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return listaPrecios;
 	}
+	
 }
--- Movil/SFH-Paciente/src/cl/sfh/paciente/ListaPrecios.java
+++ Movil/SFH-Paciente/src/cl/sfh/paciente/ListaPrecios.java
@@ -1,11 +1,15 @@
 package cl.sfh.paciente;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,52 +18,68 @@ import cl.sfh.controlaodas.ControladoraListaPrecios;
 import cl.sfh.entidades.ListaPreciosEntidad;
 import cl.sfh.libreria.ItemAdapter;
 
-public class ListaPrecios extends Activity implements
-		AdapterView.OnItemClickListener
+public class ListaPrecios extends Activity implements OnClickListener
 {
 	private ListView listaMain;
+	private EditText txtBuscar;
+	private Button btnBuscar;
+	private ProgressDialog dialogo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lista_precios);
+		dialogo = new ProgressDialog(ListaPrecios.this);
+		dialogo.setMessage("Cargando Lista Precio");
+		dialogo.setTitle("Cargando");
+		dialogo.show();
 		inicializarElementos();
 	}
 
 	private void inicializarElementos()
 	{
+		txtBuscar = (EditText)findViewById(R.id.txtBusqueda);
+		btnBuscar = (Button)findViewById(R.id.btnBuscar);
 		listaMain = (ListView) findViewById(R.id.lstPrecios);
+		btnBuscar.setOnClickListener(this);
+		
 		new Asincrono(listaMain).execute();
 	}
-
 	@Override
-	public boolean onCreateOptionsMenu(android.view.Menu menu)
+	public void onClick(View v)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		switch (v.getId())
+		{
+			case R.id.btnBuscar:
+				dialogo.show();
+				new BusquedaAsincrona(listaMain,txtBuscar).execute();
+				break;
+		}
 	}
-
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position,
-			long id)
-	{
-
-	}
-
+	
 	class Asincrono extends
 			AsyncTask<String, String, ArrayList<ListaPreciosEntidad>>
 	{
+		
 		ListView lista;
+		
 		public Asincrono(ListView lista)
 		{
+			
+			
 			this.lista = lista;
+		}
+		
+		@Override
+		protected void onPreExecute()
+		{
+			// TODO Auto-generated method stub
+			super.onPreExecute();
 		}
 
 		@Override
-		protected ArrayList<ListaPreciosEntidad> doInBackground(
-				String... params)
+		protected ArrayList<ListaPreciosEntidad> doInBackground(String... params)
 		{
 			ControladoraListaPrecios precios = new ControladoraListaPrecios();
 			ArrayList<ListaPreciosEntidad> listarPrecio = precios
@@ -70,11 +90,43 @@ public class ListaPrecios extends Activity implements
 		@Override
 		protected void onPostExecute(ArrayList<ListaPreciosEntidad> result)
 		{
+			dialogo.dismiss();
 			ItemAdapter adapter = new ItemAdapter(ListaPrecios.this, result);
 			lista.setAdapter(adapter);
-			//lista.setOnItemClickListener(ListaPrecios.this);
+			
 			super.onPostExecute(result);
 		}
 
 	}
+	class BusquedaAsincrona extends AsyncTask<String, String, ArrayList<ListaPreciosEntidad>>
+	{
+		ListView lista;
+		EditText texto;
+		public BusquedaAsincrona(ListView lista,EditText texto)
+		{
+			this.lista = lista;
+			this.texto = texto;
+		}
+
+		@Override
+		protected ArrayList<ListaPreciosEntidad> doInBackground(String... params)
+		{
+			ControladoraListaPrecios precios = new ControladoraListaPrecios();
+			ArrayList<ListaPreciosEntidad> listarPrecio = precios.listarPreciosNombres(
+																	texto.getText().toString());
+			return listarPrecio;
+		}
+
+		@Override
+		protected void onPostExecute(ArrayList<ListaPreciosEntidad> result)
+		{
+			dialogo.dismiss();
+			ItemAdapter adapter = new ItemAdapter(ListaPrecios.this, result);
+			lista.setAdapter(adapter);
+			super.onPostExecute(result);
+		}
+
+	}
+	
+
 }
