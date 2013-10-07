d0b2e750bc53ffd01925096769d59a1290a314c7
Jaime Acevedo <jaim.acevedoc@gmail.com>
2013-10-07 00:12:21 -0300

    Termino modificacion de datos
    Se sube las modificaciones de los datos, se integro el evento on y off

--- web/asistente/javascript/scriptListaPrecios.js
+++ web/asistente/javascript/scriptListaPrecios.js
@@ -8,6 +8,8 @@ function inicializarEventos()
 }
 function buscarPor()
 {
+
+	$("#tablaListaPrecios").off("click",".btnEditarPrecio",modificarObjeto);
 	var concepto = $("#txtBuscaTratamiento").val();
 	var precios = direccionWeb+"ws-precios-insumos.php";
 	var data = {"send":"{\"indice\":4,\"nombre\":\""+concepto+"\"}"};
@@ -19,9 +21,100 @@ function buscarPor()
 				tabla = tabla+"<tr><td>"+this.idPrecios+"</td><td>"+this.comentario+"</td><td>"+this.valorTotal+"</td><td><Button class='btnEditarPrecio'>Editar</Button></td></tr>";
 			});
 		$("#cuerpoTabla").html(tabla);
+
+
 		//var obj2 = jQuery.parseJSON(obj.listaPrecios[1]);
 		//alert(obj.listaPrecios[0].valorNeto);
 	});
+	$("#tablaListaPrecios").on("click",".btnEditarPrecio",modificarObjeto);
+}
+function modificarObjeto()
+{
+	$("#tablaListaPrecios").off("click",".btnEditarPrecio",modificarObjeto);
+
+	if($(this).html()=="Editar")
+	{
+		var idCosto;
+		$(this).parent().parent().children().each(function(i)
+		{
+			var texto = $(this).html();
+			if(i==0)
+			{
+				idCosto = $(this).html();
+			}
+			else if(i==1)
+			{
+				var valor = $(this).html();
+				var input = "<input type='text' id='concepto-"+idCosto+"'  style='width: 80%;' />";
+				$(this).html(input);
+				$("#concepto-"+idCosto).val(valor);
+
+			}
+			else if(i==2)
+			{
+				var valor = $(this).html();
+				var input = "<input type='text' id='costo-"+idCosto+"'  style='width: 80%;' />";				
+				$(this).html(input);
+				$("#costo-"+idCosto).val(valor);
+			}
+			else if(i==3)
+			{
+				$(this).html("<Button class='btnEditarPrecio'>Guardar</Button>");
+			}
+		});
+	}
+	else
+	{
+		var idCosto;
+		var concepto;
+		var costo;
+		$(this).parent().parent().children().each(function(i)
+		{
+			var texto = $(this).html();
+			if(i==0)
+			{
+				idCosto = $(this).html();
+			}
+			else if(i==1)
+			{
+				concepto = $("#concepto-"+idCosto).val();
+				$(this).html(concepto);
+			}
+			else if(i==2)
+			{
+				costo = $("#costo-"+idCosto).val();
+				$(this).html(costo);
+			}
+			else if(i==3)
+			{
+				$(this).html("<Button class='btnEditarPrecio'>Editar</Button>");
+			}
+		});
+
+		var precios = direccionWeb+"ws-precios-insumos.php";
+		var data = {"send":"{\"indice\":2,\"idPrecio\":"+idCosto+",\"Comentario\":\""+concepto+"\",\"ValorNeto\":"+costo+"}"};
+		$.post(precios,data,function(datos)
+		{
+			var obj = $.parseJSON(datos);
+			var resultado = obj.Modificado;
+			if(resultado=="Modificado")
+			{
+				alert("sus datos fueron modificados correctamente");
+			}
+			else
+			{
+				alert("no se produjo ninguna modificacion")
+			}
+		});
+		
+	}
+	$("#tablaListaPrecios").on("click",".btnEditarPrecio",modificarObjeto);
+}
+function insertar(idCosto,concepto,costo)
+{
+	//{"indice":2,"idPrecio":2,"Comentario":"asd","ValorNeto":130}
+	
+	
 }
 function cargarListaPrecios()
 {
@@ -38,5 +131,6 @@ function cargarListaPrecios()
 		//var obj2 = jQuery.parseJSON(obj.listaPrecios[1]);
 		//alert(obj.listaPrecios[0].valorNeto);
 	});
+	$("#tablaListaPrecios").on("click",".btnEditarPrecio",modificarObjeto);
 	
 }
