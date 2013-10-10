5aa134df2ad6de574585351d19723e0a9715fc99
LittlePhoenix8 <vivianagarridos@gmail.com>
2013-10-10 17:59:14 -0300

    Login web
    login web

---
+++ web/paciente/javascript/scriptLogin.js
@@ -0,0 +1,30 @@
+var direccionWeb = "http://localhost/proyectoTitulo/sfhwebservice/webService/";
+$(document).ready(inicializarEventos);
+
+function inicializarEventos()
+{
+	$("#btnIngresar").click(login);
+}
+
+function login()
+{
+	var usuario = $("#txtUsuario").val();
+	var contrasena = $("#txtPass").val();
+
+	var ingresar = direccionWeb+"ws-login.php";
+	var data = {"send":"{\"indice\":1,\"usuario\":"+usuario+",\"pass\":\""+contrasena+"\"}"};
+
+	$.post(ingresar, data, function(datos){
+		//alert(datos);
+		var obj = $.parseJSON(datos);
+		var codigo = obj.codAcceso;
+		if(codigo == 704)
+		{
+			alert("OK");
+		}
+		else
+		{
+			alert("Usuario y/o Contraseña erroneo.");
+		}
+	});
+}
