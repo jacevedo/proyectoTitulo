d9103e5d817dbe434a98893be245a66a1b24ef4a
Jaime Acevedo <jaim.acevedoc@gmail.com>
2013-10-10 13:46:14 -0300

    Login
    Subo Web service Login

--- sfhwebservice/controladoras/controladoralogin.php
+++ sfhwebservice/controladoras/controladoralogin.php
@@ -1,11 +1,12 @@
 <?php
 require_once '../bdmysql/MySqlCon.php'; 
-require_once '../pojos/pass.php';
+require_once '../pojos/session.php';
 
 class ControladoraLogin
 {
 	private $SqlQuery;
 	private $datos;
+
 
 	public function validarUsusario($usuario, $pass)
 	{
@@ -22,10 +23,19 @@ class ControladoraLogin
         	$sentencia->execute();
         	if($sentencia->fetch())
         	{
+        		$conexion->close();
         		$hasher = new PasswordHash(8, false);	
         		if($hasher->CheckPassword($pass, $passBD))
 		        {
-		        	$datos = insertSession($idPersona, $usuarioBD, $passBD);
+		        	$session = new Session();
+					date_default_timezone_set('America/Argentina/Buenos_Aires');
+		        	$hoy = date("Y-m-d H:i:s",time());
+		        	$keyDesencriptada = $idPersona.$hoy;
+		        	$keyHashada = $hasher->HashPassword($keyDesencriptada);
+		        	$session->initClass(0, $idPersona, $keyHashada,$hoy,null);
+		        	$datos = $this->insertSession($session);
+		        	
+
 		        }			
 		        else
 		        {
@@ -34,7 +44,7 @@ class ControladoraLogin
 				$indice=0;     
 				
       		}
-       		$conexion->close();
+       		
 		}
 		catch(Exception $e)
 		{
@@ -42,7 +52,43 @@ class ControladoraLogin
 		}
 		return $datos;
 	}
-	function insertSession($idPersona, $usuarioBD, $passBD)
-	
+	private function insertSession(Session $session)
+	{
+		$conexion = new MySqlCon();
+		$idPersona = $session->idPersona;
+		$keyHashada = $session->keySession;
+		$horaFechaIngreso = $session->fechaHoraIngreso;
+		$datos = '';
+		try
+		{
+			$this->SqlQuery='';
+	        $this->SqlQuery="INSERT INTO session (ID_SESSION, ID_PERSONA, KEY_SESSION, FECHA_HORA_INGRESO, FECHA_HORA_CADUCIDAD) VALUES (NULL ,?,?,?,?)";
+			$sentencia=$conexion->prepare($this->SqlQuery);
+	        $sentencia->bind_param('isss', $idPersona, $keyHashada,$horaFechaIngreso,$horaFechaIngreso);
+	      	if($sentencia->execute())
+	      	{
+	        	$conexion->close();
+				if($sentencia->insert_id!=-1)
+				{
+					$datos = $keyHashada;
+				}
+				else
+				{
+					$datos = "Error en el inicio de session";
+				}
+			}
+			else
+			{
+				$conexion->close();
+	        	$datos = "Error en el inicio de session";
+	        }
+
+		}
+		catch(Exception $e)
+		{
+			throw new $e("Error al listar ordenes");
+		}
+		return $datos;
+	}
 }
 ?>
