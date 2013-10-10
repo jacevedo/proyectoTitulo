ae6008a8a505681eaf83cfce5b31c5daa79ba78e
LittlePhoenix8 <vivianagarridos@gmail.com>
2013-10-10 16:37:03 -0300

    Web service login

--- sfhwebservice/webService/ws-login.php
+++ sfhwebservice/webService/ws-login.php
@@ -26,21 +26,5 @@ switch ($opcion)
 
 
 		echo(json_encode($arreglo));
-	break;
-	case 2:
-		//json Modificar Tratamiento Dental {"indice":2,"idTratamientoDental":1,"idFicha":1,"fechaCreacion":"1991-12-12","tratamiento":"extraccion","fechaSeguimiento":"1991-12-12","valorTotal":100000}
-		$idTratamientoDental = $data->{"idTratamientoDental"};
-		$idFicha = $data->{'idFicha'};
-		$fechaCreacion = $data->{'fechaCreacion'};
-		$tratamiento = $data->{'tratamiento'};
-		$fechaSeguimiento = $data->{'fechaSeguimiento'};
-		$valorTotal = $data->{'valorTotal'};
-		$tratamientoDental = new TratamientoDental();
-		$tratamientoDental->initClass($idTratamientoDental, $idFicha, $fechaCreacion,$tratamiento, $fechaSeguimiento,$valorTotal);
-		$controladoraFicha = new ControladoraTratamientoAbono();
-		$arreglo["code"]=2;
-		$arreglo["Resultado"] = $controladoraFicha->modificarTratamiento($tratamientoDental);
-		echo(json_encode($arreglo));
-	break;
-	
+	break;	
 }
